8aabdf6dafe00cab19dba865925ccd48f4fea21e
Wilker Lucio <wilkerlucio@gmail.com>
2016-05-25 12:09:36 -0300

    removed hello.jsx

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import Hello from './src/hello.jsx';
+import './src/styles';
+import Intervals from './src/exercices/intervals.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement(Hello, null), document.getElementById('app-container'));
+ReactDOM.render(React.createElement(Intervals, null), document.getElementById('app-container'));
